f03d3ee299c6a41a5ac8c1a9e004b072ca1438e7
KolesnikovaJulia <julia_tea@mail.ru>
2022-01-12 10:22:35 +0100

    Домашнее задание # 6 05.01

---
+++ src/main/java/com/geekbrains/Cat.java
@@ -0,0 +1,7 @@
+package com.geekbrains;
+
+public class Cat extends Animal {
+    public Cat(String name, int run, int swim) {
+        super(name, run, swim);
+    }
+}
---
+++ src/main/java/com/geekbrains/Dog.java
@@ -0,0 +1,7 @@
+package com.geekbrains;
+
+public class Dog extends Animal {
+    public Dog(String name, int run, int swim) {
+        super(name, run, swim);
+    }
+}
